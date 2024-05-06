6741348ccdf7b2d722fac4d93a6005cf5e7ff0d9
elim.poon <elim.poon@okg.com>
2024-05-06 11:10:47 +0800

    deployment tests

---
+++ src/Factory.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import {Proxy} from "@openzeppelin/contracts/proxy/Proxy.sol";
+import {ERC1967Utils} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol";
+import {LibClone} from "solady/utils/LibClone.sol";
+
+contract Factory {
+    bytes32 constant SALT = keccak256(abi.encode("hello"));
+
+    function getTstoreImpl() external view returns (address impl) {
+        assembly {
+            impl := tload(0)
+        }
+    }
+
+    function createTstore(address impl) external {
+        assembly {
+            tstore(0, impl)
+        }
+        new TstoreProxy{salt: SALT}();
+    }
+
+    function updateTemp(address actualImpl) external {
+        ERC1967Utils.upgradeToAndCall(actualImpl, "");
+    }
+
+    // Self as temp init impl OZ
+    function createTempOZ(address impl) external {
+        Factory proxy = Factory(address(new TempOzProxy(address(this))));
+        proxy.updateTemp(impl);
+    }
+
+    // self as temp init impl solady
+    function createTempSol(address impl) external {
+        (, address proxy) = LibClone.createDeterministicERC1967(
+            address(this),
+            SALT
+        );
+        Factory(proxy).updateTemp(impl);
+    }
+}
+
+contract TstoreProxy is Proxy {
+    constructor() {
+        address implementation = Factory(msg.sender).getTstoreImpl();
+        ERC1967Utils.upgradeToAndCall(implementation, "");
+    }
+
+    function _implementation()
+        internal
+        view
+        virtual
+        override
+        returns (address)
+    {
+        return ERC1967Utils.getImplementation();
+    }
+}
+
+contract TempOzProxy is Proxy {
+    constructor(address implementation) {
+        ERC1967Utils.upgradeToAndCall(implementation, "");
+    }
+
+    function _implementation()
+        internal
+        view
+        virtual
+        override
+        returns (address)
+    {
+        return ERC1967Utils.getImplementation();
+    }
+}
---
+++ test/Factory.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Factory} from "../src/Factory.sol";
+
+contract FactoryTest is Test {
+    address immutable impl = address(this);
+    Factory public factory;
+
+    function setUp() public {
+        factory = new Factory();
+    }
+
+    function test_Tstore() public {
+        factory.createTstore(impl);
+    }
+
+    function test_TempOz() public {
+        factory.createTempOZ(impl);
+    }
+
+    function test_TempSol() public {
+        factory.createTempSol(impl);
+    }
+}
